5aba586869fe6b4b5c4a189ddf3da475143a039f
깃waimi3169 <waimi3169@naver.com>
2023-04-12 10:12:35 +0900

    LikeablePersonServiceTests 작성(강ã사님 힌트)

---
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonServiceTests.java
@@ -0,0 +1,42 @@
+package com.ll.gramgram.boundedContext.likeablePerson.service;
+
+
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+public class LikeablePersonServiceTests {
+    @Autowired
+    private LikeablePersonService likeablePersonService;
+
+    @Test
+    @DisplayName("테스트 1")
+    void t001() throws Exception {
+
+        LikeablePerson likeablePersonId2 = likeablePersonService.findById(2L).get();
+
+
+        InstaMember instaMemberInstaUser3 = likeablePersonId2.getFromInstaMember();
+        assertThat(instaMemberInstaUser3.getUsername()).isEqualTo("insta_user3");
+
+
+        List<LikeablePerson> fromLikeablePeople = instaMemberInstaUser3.getFromLikeablePeople();
+
+        // 특정 회원이 호감을 표시한 좋아요 반복한다.
+        for (LikeablePerson likeablePerson : fromLikeablePeople) {
+            assertThat(instaMemberInstaUser3.getUsername()).isEqualTo(likeablePerson.getFromInstaMember().getUsername());
+        }
+    }
+}

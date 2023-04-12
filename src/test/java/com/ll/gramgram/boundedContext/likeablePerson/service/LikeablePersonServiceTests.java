c3095604e3c5f193e51815f001cf2bd283e08e45
깃waimi3169 <waimi3169@naver.com>
2023-04-12 10:14:04 +0900

    어떠한 회원이 이미 누가 나를 좋아하는지 테스트(강사님 힌트2)

--- src/test/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonServiceTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonServiceTests.java
@@ -39,4 +39,63 @@ public class LikeablePersonServiceTests {
             assertThat(instaMemberInstaUser3.getUsername()).isEqualTo(likeablePerson.getFromInstaMember().getUsername());
         }
     }
+
+    @Test
+    @DisplayName("테스트 2")
+    void t002() throws Exception {
+
+        LikeablePerson likeablePersonId2 = likeablePersonService.findById(2L).get();
+
+        InstaMember instaMemberInstaUser3 = likeablePersonId2.getFromInstaMember();
+        assertThat(instaMemberInstaUser3.getUsername()).isEqualTo("insta_user3");
+
+        String usernameToLike = "insta_user4";
+
+        // v1
+        LikeablePerson likeablePersonIndex0 = instaMemberInstaUser3.getFromLikeablePeople().get(0);
+        LikeablePerson likeablePersonIndex1 = instaMemberInstaUser3.getFromLikeablePeople().get(1);
+
+        if (usernameToLike.equals(likeablePersonIndex0.getToInstaMember().getUsername())) {
+            System.out.println("v1 : 이미 나(인스타아이디 : insta_user3)는 insta_user4에게 호감을 표시 했구나.");
+        }
+
+        if (usernameToLike.equals(likeablePersonIndex1.getToInstaMember().getUsername())) {
+            System.out.println("v1 : 이미 나(인스타아이디 : insta_user3)는 insta_user4에게 호감을 표시 했구나.");
+        }
+
+        // v2
+        for (LikeablePerson fromLikeablePerson : instaMemberInstaUser3.getFromLikeablePeople()) {
+            String toInstaMemberUsername = fromLikeablePerson.getToInstaMember().getUsername();
+
+            if (usernameToLike.equals(toInstaMemberUsername)) {
+                System.out.println("v2 : 이미 나(인스타아이디 : insta_user3)는 insta_user4에게 호감을 표시 했구나.");
+                break;
+            }
+        }
+
+        // v3
+        long count = instaMemberInstaUser3
+                .getFromLikeablePeople()
+                .stream()
+                .filter(lp -> lp.getToInstaMember().getUsername().equals(usernameToLike))
+                .count();
+
+        if (count > 0) {
+            System.out.println("v3 : 이미 나(인스타아이디 : insta_user3)는 insta_user4에게 호감을 표시 했구나.");
+        }
+
+        // v4
+        LikeablePerson oldLikeablePerson = instaMemberInstaUser3
+                .getFromLikeablePeople()
+                .stream()
+                .filter(lp -> lp.getToInstaMember().getUsername().equals(usernameToLike))
+                .findFirst()
+                .orElse(null);
+
+        if (oldLikeablePerson != null) {
+            System.out.println("v4 : 이미 나(인스타아이디 : insta_user3)는 insta_user4에게 호감을 표시 했구나.");
+            System.out.println("v4 : 기존 호감사유 : %s".formatted(oldLikeablePerson.getAttractiveTypeDisplayName()));
+        }
+    }
 }
+

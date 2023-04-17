bec32aa81d1cafee073269c969fab47777b08015
깃waimi3169 <waimi3169@naver.com>
2023-04-17 09:20:06 +0900

    LikeableServiceTests 구현 (강사님)

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -4,7 +4,6 @@ import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Long> {
     List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId);
@@ -12,6 +11,5 @@ public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, 
     List<LikeablePerson> findByToInstaMember_username(String username);
 
     LikeablePerson findByFromInstaMemberIdAndToInstaMember_username(long fromInstaMemberId, String username);
-
-    Optional<LikeablePerson> findByToInstaMemberUsername(String username);
 }
+

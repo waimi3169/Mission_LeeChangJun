6603de3432e0d6b081195a1df43514de0ae71935
깃waimi3169 <waimi3169@naver.com>
2023-05-03 11:13:01 +0900

    쿨타임 구현

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
@@ -38,7 +39,14 @@ public class LikeablePerson extends BaseEntity {
 
     // 초 단위에서 올림 해주세요.
     public String getModifyUnlockDateRemainStrHuman() {
-        return "2시간 16분";
+        LocalDateTime now = LocalDateTime.now();
+        Duration duration = Duration.between(now, modifyUnlockDate);
+
+        long hour = duration.toHours();
+        long minutes = duration.toMinutes() % 60;
+        long seconds = duration.getSeconds() % 60;
+
+        return "%s시간 %s분 %s초".formatted(hour, minutes, seconds);
     }
 
     public RsData updateAttractionTypeCode(int attractiveTypeCode) {
